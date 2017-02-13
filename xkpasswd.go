50ae3a241ad6ebb874efa168c97c1a5188264920
Vot <vzardzewialy@cambridge.org>
2017-02-13 11:31:17 +0000

    Added CLI parsing

--- xkpasswd.go
+++ xkpasswd.go
@@ -3,10 +3,13 @@ package main
 import "fmt"
 import "log"
 import "time"
-import "math/rand"
-
-import "io/ioutil"
 import "strings"
+import "os"
+
+import "math/rand"
+import "io/ioutil"
+
+import "github.com/urfave/cli"
 
 func readLinesSimple(path string) ([]string, error) {
   content, err := ioutil.ReadFile(path)
@@ -38,10 +41,35 @@ func getRandomWord () (string) {
   return allWords[randomNumber]
 }
 
-func main() {
+func generatePassword (compl int) (string) {
   separator := "#"
   s := []string{getRandomWord(), getRandomWord(), getRandomWord(), getRandomWord()}
   var output string = strings.Join(s, separator)
-  fmt.Println(output)
+  return output
 }
 
+// TODO download list of words if missing
+func main() {
+  app := cli.NewApp()
+  app.Name = "xkpasswd"
+  app.Usage = "Generate memorable passwords"
+
+  var complexity int
+
+  app.Flags = []cli.Flag {
+    cli.IntFlag{
+      Name:        "complexity, c",
+      Value:       3,
+      Usage:       "Define complexity (1-5)",
+      Destination: &complexity,
+    },
+  }
+
+  app.Action = func(c *cli.Context) error {
+    var output string = generatePassword(complexity)
+    fmt.Println(output)
+    return nil
+  }
+
+  app.Run(os.Args)
+}
